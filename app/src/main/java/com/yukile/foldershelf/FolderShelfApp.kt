e9631555eba183453232a337707c75fdd8f2ac71
Yukile394 <muhamedmuhet@gmail.com>
2026-08-02 14:09:27 +0300

    Add CrashHandler installation in onCreate
    Install CrashHandler during application initialization.

--- app/src/main/java/com/yukile/foldershelf/FolderShelfApp.kt
+++ app/src/main/java/com/yukile/foldershelf/FolderShelfApp.kt
@@ -3,23 +3,17 @@ package com.yukile.foldershelf
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.color.DynamicColors
+import com.yukile.foldershelf.util.CrashHandler
 import com.yukile.foldershelf.util.PreferenceHelper
 
-/**
- * FolderShelfApp
- *
- * Uygulama genelinde bir kez calismasi gereken kurulum islemlerini
- * (Material You dinamik renk destegi, kaydedilmis tema tercihinin
- * uygulanmasi) burada yapariz.
- */
 class FolderShelfApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-        // Android 12+ (API 31+) cihazlarda duvar kagidina gore dinamik
-        // renklendirme uygular; daha eski surumlerde guvenli sekilde
-        // hicbir sey yapmaz.
+        // EN ONCE kurulmali
+        CrashHandler.install(this)
+
         DynamicColors.applyToActivitiesIfAvailable(this)
 
         applySavedThemeMode()
